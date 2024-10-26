7858a156ccad86a6683925690a75b8775840d9d6
David Choi <dharric@live.com>
2024-10-25 21:06:00 -0400

    update validation include password

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::{post, web::Json, App, HttpResponse, HttpServer};
 use serde::Deserialize;
-use validator::Validate;
+use validator::{Validate, ValidationError};
 
 #[allow(unused)]
 #[derive(Deserialize, Validate)]
@@ -8,10 +8,22 @@ struct User {
     #[validate(length(min = 5, max = 10))]
     #[serde(rename = "userName")]
     user_name: String,
+    #[validate(length(min = 10), custom(function = "is_valid_password"))]
+    password: String,
     #[validate(email)]
     email: String,
     #[validate(range(min = 18))]
     age: i32
+}
+
+fn is_valid_password(password: &str) -> Result<(), ValidationError> {
+    let has_numbers = regex::Regex::new(r"\d").unwrap();
+    let has_symbols = regex::Regex::new(r#"[!@#\$%\^&\*\(\),\.\?\":{}|<>]"#).unwrap();
+
+    match has_numbers.is_match(&password) && has_symbols.is_match(&password) {
+        true => Ok(()),
+        false => Err(ValidationError::new("Password is invalid"))
+    }
 }
 
 #[post("/new_user")]
@@ -32,45 +44,3 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await
 }
-
-// use actix_web::{post, web::Json, App, HttpResponse, HttpServer};
-// use serde::Deserialize;
-// use validator::Validate;
-
-// #[allow(unused)]
-// #[derive(Deserialize, Debug, Validate)]
-// struct User {
-//     #[validate(length(min = 5, max = 15))]
-//     #[serde(rename = "userName")]
-//     user_name: String,
-//     #[validate(length(min = 8, max = 15))]
-//     #[serde(rename = "fullName")]
-//     full_name: String,
-//     #[validate(range(min = 18))]
-//     age: i32,
-//     #[validate(email)]
-//     email: String
-// }
-
-// #[post("/new")]
-// async fn new_user(user: Json<User>) -> HttpResponse {
-//     match user.0.validate() {
-//         Ok(_) => {
-//             println!("user: {:?}", user);
-
-//             HttpResponse::Created().into()
-//         },
-//         Err(e) => HttpResponse::InternalServerError().json(e.errors())
-//     }    
-// }
-
-// #[tokio::main]
-// async fn main() -> std::io::Result<()> {
-//     HttpServer::new(|| {
-//         App::new()
-//             .service(new_user)
-//     })
-//     .bind(("0.0.0.0", 4000)).expect("Failed to bind to host and port")
-//     .run()
-//     .await
-// }
